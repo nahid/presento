a5f44aa94aad5f318de0742e2398d3f103d6e7ad
me-shaon <shaon.cse81@gmail.com>
2019-09-20 16:20:31 +0600

    feat: move the 'blank' method to helpers

--- helpers/presento.php
+++ helpers/presento.php
@@ -68,3 +68,33 @@ if (!function_exists('is_collection')) {
         return isset($first) && is_array($first);
     }
 }
+
+
+if (!function_exists('blank')) {
+    /**
+     * Check if the given value is null or empty
+     *
+     * @param $value
+     * @return bool
+     */
+    function blank($value): bool
+    {
+        if (is_null($value)) {
+            return true;
+        }
+
+        if (is_string($value)) {
+            return trim($value) === '';
+        }
+
+        if (is_numeric($value) || is_bool($value)) {
+            return false;
+        }
+
+        if ($value instanceof Countable) {
+            return count($value) === 0;
+        }
+
+        return empty($value);
+    }
+}
